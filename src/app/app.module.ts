f856067a63bc9cff80cb40d901c8c101063674ab
lorped <lorped@gmail.com>
2026-07-28 19:18:11 +0200

    angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { User, Oggetto, ToChange } from './globals';
 import { PipesModule } from './pipes/pipes.module';
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, PipesModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, User, Oggetto, BarcodeScanner, InAppBrowser, ToChange],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, PipesModule], providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, User, Oggetto, BarcodeScanner, InAppBrowser, ToChange, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
